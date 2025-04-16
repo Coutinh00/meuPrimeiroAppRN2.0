d1d6d92e6612268f571ace604884d89593e07896
Coutinh00 <lucasconsentino57@gmail.com>
2025-04-16 01:42:07 -0300

    Aula 20/03 finalizada

--- MeuAppReactNative/App.tsx
+++ MeuAppReactNative/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigaito } from '@react-navigation/native-stack'
-import HomeScreen from './src/screens/HomesScreen';
-import { SafeAreaView, StatusBar } from 'react-native';
-
-const Stack = createNativeStackNavigaito();
+import { ThemeProvider } from 'styled-components';
+import AppRoutes from './src/routes';
+import theme from './src/styles/theme';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar barStyle="light-content" backgroundColor="#007BFF" />
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </SafeAreaView>
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StatusBar 
+          barStyle="light-content" 
+          backgroundColor={theme.colors.primary} 
+        />
+        <AppRoutes />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
